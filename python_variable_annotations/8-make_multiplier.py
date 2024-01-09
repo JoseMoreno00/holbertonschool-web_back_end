c290cd18bd282d55adf494656907f281d072712c
JoseMoreno00 <accgitjosem@gmail.com>
2024-01-09 08:29:00 -0800

    type-annotated function make_multiplier that takes a float

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Write a type-annotated function make_multiplier that takes a float
+"""type-annotated function make_multiplier that takes a float
 multiplier as argument and returns a function that multiplies a float
 by multiplier"""
 from typing import Callable
