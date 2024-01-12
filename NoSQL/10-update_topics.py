9a7dd317063b335b4436e5101e46d4aecf71e40c
JoseMoreno00 <accgitjosem@gmail.com>
2024-01-12 09:00:12 -0800

    Update | I put an & in set instead of $, I'm fine.

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -7,4 +7,4 @@ import pymongo
 
 def update_topics(mongo_collection, name, topics):
     """Function"""
-    mongo_collection.update_many({"name":name}, {"&set": {"topics": topics}})
+    mongo_collection.update_many({"name":name}, {"$set": {"topics": topics}})
