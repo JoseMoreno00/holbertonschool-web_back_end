46faf690b407b493bef364e2c8f4d3e55d3a435d
JoseMoreno00 <accgitjosem@gmail.com>
2024-01-12 11:33:35 -0800

    script that provides some stats about Nginx logs stored in MongoDB:

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,7 +3,8 @@
 script that provides some stats about Nginx logs stored in MongoDB
 """
 from pymongo import MongoClient
-
+list_all = __import__('8-all').list_all
+insert_school = __import__('9-insert_school').insert_school
 
 if __name__ == "__main__":
     server = MongoClient("mongodb://127.0.0.1:27017")
