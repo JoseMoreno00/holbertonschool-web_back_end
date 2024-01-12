185567f2e7e65bef5bd4078ba5bdc0ddcd1d634c
JoseMoreno00 <accgitjosem@gmail.com>
2024-01-12 14:54:27 -0800

    method named get_page that takes two integer arguments page with default value 1 and page_size with default value 10.

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -7,6 +7,7 @@ import csv
 import math
 from typing import List, Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Function"""
     return ((page - 1) * page_size, page * page_size)
@@ -32,8 +33,10 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert type(page) == int and page >= 1
-        assert type(page_size) == int and page_size >= 1
+        """method named get_page that takes two integer arguments page with
+default value 1 and page_size with default value 10."""
+        assert type(page) is int and page >= 1
+        assert type(page_size) is int and page_size >= 1
         pag = index_range(page, page_size)
         self.dataset()
         return self.__dataset[pag[0]:pag[1]]
