1628ca133da6a70075001ad9767df09c79300767
JoseMoreno00 <accgitjosem@gmail.com>
2024-01-23 20:14:13 -0800

    Async Comprehension

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -9,7 +9,5 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """function"""
-    numbers = []
-    async for i in async_generator():
-        numbers.append(i)
+    numbers = [i async for i in async_generator()]
     return numbers
