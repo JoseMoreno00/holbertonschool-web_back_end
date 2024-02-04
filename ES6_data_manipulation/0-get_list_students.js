8574f914fc725d02a8acda0d28f7313efddb1ef7
JoseMoreno00 <accgitjosem@gmail.com>
2024-02-04 15:40:27 -0800

    Function named getListStudents that returns an array of objects.

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudentsa() {
+  const array = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return array;
+}
