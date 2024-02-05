1d6d433af86fe08ea25010d3a4baf84fc8f8cfc4
JoseMoreno00 <accgitjosem@gmail.com>
2024-02-05 13:14:36 -0800

    4. Pricing

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') throw TypeError('amount must be a number');
+    if (!(currency instanceof Currency)) throw TypeError('currency must be a Currency');
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
