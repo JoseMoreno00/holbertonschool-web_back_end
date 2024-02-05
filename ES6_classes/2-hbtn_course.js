fb0039034c2014a494c2abe49fa10b0b44953b1e
JoseMoreno00 <accgitjosem@gmail.com>
2024-02-05 11:50:12 -0800

    2. A Course, Getters, and Setters

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,7 +1,19 @@
 export default class HolbertonCourse {
-    constructor(name, length, students) {
-        this._name = name
-        this._length = length
-        this._students = students
-    }
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  set name(name) {
+    this._name = name;
+  }
+
+  set length(length) {
+    this._length = length;
+  }
+
+  set students(students) {
+    this._students = students;
+  }
 }
