deea2015af297c604bb17373b8ed8084830364fe
JoseMoreno00 <accgitjosem@gmail.com>
2024-02-05 12:13:44 -0800

    if (Array.isArray(students) !== 'true') throw TypeError('Students must be a array of strings');

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,19 +1,37 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    if (typeof length !== 'number') throw TypeError('Length must be a number');
+    if (!Array.isArray(students)) throw TypeError('Students must be a array of strings');
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
   }
 
   set name(name) {
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
     this._name = name;
   }
 
   set length(length) {
+    if (typeof length !== 'number') throw TypeError('Length must be a number');
     this._length = length;
   }
 
   set students(students) {
+    if (!Array.isArray(students)) throw TypeError('Students must be a array of strings');
     this._students = students;
   }
 }
